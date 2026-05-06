df0a6640ae612f682482087c64cd1fd1545063eb
ny-fabian-b <fabian.blatter@noseryoung.ch>
2026-05-06 10:59:31 +0200

    use Customer instead of SnackMachine

---
+++ noseryoung/ch/src/Customer.java
@@ -0,0 +1,11 @@
+
+
+/* SnackMachine was renamed to Customer. the SnackMachine class is not needed,
+ * and i think its better to let a Customer class interface with the
+ * SnackInventory and the CLI with the Customer class. this way,
+ * you dont need a seperate class to manage the prices or manage
+ * them in the CLI which doesnt make sense.
+ * */
+public class Customer {
+    
+}
