3b4ba5ca5cef082ec08f00952888ece40079067a
ny-fabian-b <fabian.blatter@noseryoung.ch>
2026-05-06 13:52:55 +0200

    add IDs to snacks

--- noseryoung/ch/src/Snack.java
+++ noseryoung/ch/src/Snack.java
@@ -1,14 +1,19 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Snack {
     private String name;
     private int count;
     private int price;
     private int initialAmount;
+    private final int id;
+    private static AtomicInteger idCounter = new AtomicInteger(0);
 
     public Snack(String name, int initialAmount, int price){
        this.name= name;
        this.initialAmount = initialAmount;
        this.count = initialAmount;
        this.price = price;
+       this.id = idCounter.getAndAdd(1);
     }
 
     public String toString(){
@@ -37,6 +42,18 @@ public class Snack {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public int getInitialAmount(){
+        return initialAmount;
+    }
+
+    public void setInitialAmount(int initialAmount){
+        this.initialAmount = initialAmount;
+    }
+
+    public int getId(){
+        return id;
     }
 
 }
