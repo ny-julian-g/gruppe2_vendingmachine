47eb6492aac81ae77cf669017f30119a3f7f0998
ny-julian-g <julian.gasser@noseryoung.ch>
2026-05-07 16:38:12 +0200

    implement numpad to all methods, that need it

--- noseryoung/ch/src/NumpadDialog.java
+++ noseryoung/ch/src/NumpadDialog.java
@@ -6,8 +6,20 @@ import java.util.Optional;
 public class NumpadDialog {
 
     public static Optional<String> show(String title) {
+        return show(title, "ID", false);
+    }
+
+    public static Optional<String> show(String title, String displayLabel) {
+        return show(title, displayLabel, false);
+    }
+
+    public static Optional<String> showDecimal(String title, String displayLabel) {
+        return show(title, displayLabel, true);
+    }
+
+    private static Optional<String> show(String title, String displayLabel, boolean allowDecimal) {
         JPanel mainPanel = new JPanel(new BorderLayout(10, 10));
-        JLabel display = new JLabel("ID: ", SwingConstants.CENTER);
+        JLabel display = new JLabel(displayLabel + ": ", SwingConstants.CENTER);
         display.setFont(new Font("Monospaced", Font.BOLD, 22));
         display.setOpaque(true);
         display.setBackground(Color.BLACK);
@@ -19,7 +31,7 @@ public class NumpadDialog {
 
         ActionListener numListener = e -> {
             input.append(e.getActionCommand());
-            display.setText("ID: " + input.toString());
+            display.setText(displayLabel + ": " + input);
         };
 
         for (int i = 1; i <= 9; i++) {
@@ -28,11 +40,22 @@ public class NumpadDialog {
             buttonPanel.add(b);
         }
 
+        if (allowDecimal) {
+            JButton decimalBtn = new JButton(".");
+            decimalBtn.addActionListener(e -> {
+                if (input.indexOf(".") == -1) {
+                    input.append(".");
+                    display.setText(displayLabel + ": " + input);
+                }
+            });
+            buttonPanel.add(decimalBtn);
+        }
+
         JButton clearBtn = new JButton("X");
         clearBtn.setForeground(Color.RED);
         clearBtn.addActionListener(e -> {
             input.setLength(0);
-            display.setText("ID: ");
+            display.setText(displayLabel + ": ");
         });
         buttonPanel.add(clearBtn);
 
